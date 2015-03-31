ba1061b4048a9fc32dfe4582b345da80a7e2a642
abu aunik <abuaunik@abus-MacBook-Pro.local>
2015-03-31 18:37:29 +0600

    First Version

--- ShoeConveter/ViewController.swift
+++ ShoeConveter/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeConvertedLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let conversionConstant = 30
+        
+        //Get the value from the TextField
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        
+        //Update the converted value in the view
+        mensConvertedShoeSizeLabel.hidden = false
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+    }
+    
+    
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: UIButton) {
+        let conversionConstant = 30.5
+        
+        //Get the value from the TextField
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        
+        //Update the converted value in view
+        womensShoeSizeConvertedLabel.hidden = false
+        womensShoeSizeConvertedLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Size"
+        
+    }
 
 }
 
